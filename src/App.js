71861a143cb5f27d8e5f7686ea38d5d918d2cbc7
jimmyjianchen <jimmy_chen@brown.edu>
2023-01-02 23:40:22 -0500

    finished without nav bar

--- src/App.js
+++ src/App.js
@@ -79,44 +79,6 @@ function App() {
               added = {favorites[item.name]} setStateOfParent={updateFavorites}/>))}
         </div>
       </div>
-
-      <div className="favorites">
-        <form>
-          <div className="sorting">
-            <h3>Sort By:</h3>
-            <input class = "jss4" type = "radio" value = "productType" defaultChecked name = "sort" onClick = {() => setSort("productType")}></input>
-            <label> Product Type </label><br/>
-            <input class = "jss4" type = "radio" value = "ascending" name = "sort" onClick = {() => setSort("ascending")}></input>
-            <label> Price: Ascending </label><br/>
-            <input class = "jss4" type = "radio" value = "descending" name = "sort" onClick = {() => setSort("descending")}></input>
-            <label> Price: Descending </label><br/>
-          </div>
-
-          <div className="filtering">
-            <h3>Product Type:</h3>
-            <input type = "checkbox" value = "iPhone" onClick = {() => updateFilter("iPhone", "product_type")}/>
-            <label> iPhone </label><br/>
-            <input type = "checkbox" value = "iPad" onClick = {() => updateFilter("iPad", "product_type")}/> 
-            <label> iPad </label><br/>
-            <input type = "checkbox" value = "Notebook" onClick = {() => updateFilter("Notebook", "product_type")}/> 
-            <label> Notebook </label><br/>
-            <input type = "checkbox" value = "Desktop" onClick = {() => updateFilter("Desktop", "product_type")}/> 
-            <label> Desktop </label><br/>
-            
-            <h3>Release Time:</h3>
-            <input type = "checkbox" value = "Released in 2022" onClick = {() => updateFilter("Released in 2022", "release_time")}/> 
-            <label> Released in 2022 </label><br/>
-            <input type = "checkbox" value = "Not Released in 2022" onClick = {() => updateFilter("Not Released in 2022", "release_time")}/> 
-            <label> Not Released in 2022 </label><br/>
-          </div>
-        
-          <div>
-            <h3>Favorites:</h3>
-            <label>Total Price: ${total}</label>
-          </div>
-          <button onClick = {() => resetPage()}>{"Reset"}</button>
-        </form>
-      </div>
     </div>);
 }
 
--- src/components/item.js
+++ src/components/item.js
@@ -4,11 +4,11 @@ export default function ProductItem(props) {
       <h2>{props.info["name"]}</h2>
       <img src={props.info["image_link"]}></img>
       <ul>
-        <li>Product Type: {props.info["product_type"]}</li>
-        <li>Processor: {props.info["processor"]}</li>
-        <li>Release Time: {props.info["release_time"]}</li>
-        <li>Slogan: {props.info["slogan"]}</li>
-        <li>Price: ${props.info["price"]}</li>
+        <li>{props.info["product_type"]}</li>
+        <li>{props.info["release_time"]}</li>
+        <li>{props.info["price"]}</li>
+        <li>{props.info["slogan"]}</li>
+        <li>{props.info["processor"]}</li>
       </ul>
       <button onClick={() => props.setStateOfParent(props.info["name"], props.info["price"])}>
         {props.added === 1 ? "Remove from Favorites" : "Add to Favorites"}</button>
