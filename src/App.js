1c58d82187fbc0f286107006feab00c4f88952bc
jimmyjianchen <jimmy_chen@brown.edu>
2023-01-10 02:24:49 -0500

    finished second nav bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className = "App">
       <div className = "product-cards">
+      <div className='secondbutton'><a href="https://www.mville.edu/">Mville.edu</a> | <a href="https://www.mville.edu/my-mville">MyMville</a> | <a href="https://blackboard.mville.edu/">Blackboard</a> | <a href="https://servicehub.mville.edu/Student/Account/Login?ReturnUrl=%2fstudent">ServiceHub</a> | <a href="http://directory.mville.edu/">Campus Directory</a> | <a href="https://www.aaiscloud.com/Manhattanville/Calendars/ActivityCalendar.aspx?calendarId=31ac6080-3dcd-11e4-a0e1-997993bdf9b0">Campus Calendar</a> | <a href="https://authn.sd00.worldcat.org/wayf/metaauth-ui/cmnd/protocol/samlpost">My Library Items</a></div>
+      
       <img src="https://libapps.s3.amazonaws.com/accounts/222921/images/banner_january_22.png" 
           width="1680"></img>
         
